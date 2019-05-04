26c7c7d2d3af1ad049cb7dd17402cedecfddc055
Johannes Boehm <johannes.boehm@gmail.com>
2019-05-05 01:04:55 +0200

    Removing old files, now all clean

--- test/linkage.jl
+++ test/linkage.jl
@@ -9,6 +9,7 @@ using Test
 using RDatasets
 using Distances
 using DataFrames
+using RCall
 
 using Fastcluster
 
@@ -31,113 +32,27 @@ end
 # Start the testing. Happy testing.
 
 df = dataset("datasets", "iris")
-
-answers = Dict{Symbol,Vector{Int64}}()
-
-answers[:single] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-answers[:complete] = [1, 2, 2, 2, 1, 1, 2, 1, 2, 2, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 2, 1, 1, 1, 2, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 2, 1, 1, 2, 1, 2, 1, 1, 3, 1, 3, 2, 1, 2, 1, 2, 1, 2, 2, 1, 1, 1, 2, 3, 2, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 2, 1, 3, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 3, 1, 1, 3, 2, 3, 1, 3, 1, 1, 1, 2, 2, 1, 1, 3, 3, 1, 3, 2, 3, 1, 3, 3, 1, 1, 1, 3, 3, 3, 1, 1, 1, 3, 1, 1, 1, 3, 3, 3, 2, 3, 3, 1, 1, 1, 1, 1]
-answers[:average] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 3, 2, 2, 3, 1, 3, 2, 3, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 3, 2, 2, 3, 2, 2, 2, 3, 3, 3, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-answers[:weighted] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 2, 2, 1, 2, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 3, 2, 2, 3, 1, 3, 2, 3, 2, 2, 2, 1, 1, 2, 2, 3, 3, 2, 2, 1, 3, 2, 2, 3, 2, 2, 2, 3, 3, 3, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2]
-answers[:centroid] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 3, 2, 2, 3, 1, 3, 2, 3, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 3, 2, 2, 3, 2, 2, 2, 3, 3, 3, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-answers[:median] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 3, 2, 2, 3, 1, 3, 2, 3, 2, 2, 2, 1, 1, 2, 2, 3, 3, 2, 2, 1, 3, 2, 2, 3, 2, 1, 2, 3, 3, 3, 2, 2, 1, 3, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
-answers[:ward] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 2, 3, 2, 1, 2, 1, 1, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 1, 3, 2, 3, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 3, 2, 3, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 3]
-
-for meth in [:single, :complete, :average, :weighted, :centroid, :median]
-    println("Testing interface with method=:$(string(meth))...")
-    for k in [3]
-        # run once with R interface, and once with Fastcluster.jl interface
-        points = convert(Array{Float64,2},df[:,[:SepalWidth, :SepalLength]])
-        d = pairwise(Euclidean(), points, dims=1)
-        m,h = Fastcluster.linkage(d, meth)
-        c_fastcluster = Fastcluster.cutree(m,(length(m)>>1)+1,k)
-        #@show c_fastcluster
-        @test are_clusters_equal(c_fastcluster, answers[meth])
-    end
-    println("Ok.")
-end
-
-# for
-for meth in [:ward]
-    println("Testing interface with method=:$(string(meth))...")
-    for k in [3]
-        # run once with R interface, and once with Fastcluster.jl interface
-        points = convert(Array{Float64,2},df[:,[:SepalWidth, :SepalLength]])
-        d = pairwise(SqEuclidean(), points, dims=1)
-        m,h = Fastcluster.linkage(d, meth)
-        c_fastcluster = Fastcluster.cutree(m,(length(m)>>1)+1,k)
-        d2 = pairwise(Euclidean(), points, dims=1)
-        #@show c_fastcluster
-        @test are_clusters_equal(c_fastcluster, answers[meth])
-    end
-    println("Ok.")
-end
-
-# # if you want to test it using the R interface:
-# using RCall
 #
-# R"r = getOption(\"repos\")
-# r[\"CRAN\"] = \"http://cran.uk.r-project.org\"
-# options(repos = r)
-# rm(r)"
-# R"install.packages(\"fastcluster\")"
+# answers = Dict{Symbol,Vector{Int64}}()
 #
-# function to wrap R interface:
-# function cluster_and_cut_R(d::Array{T,2}, method::Symbol, nclusters::Int64) where {T<:Real}
-#
-#     @rput d
-#
-#     mthidx = Dict(:single => 0,
-#               :complete  => 1,
-#               :average   => 2,
-#               :weighted  => 3,
-#               :ward      => 4,
-#               :centroid  => 5,
-#               :median    => 6 )
-#
-#     # for R's fastcluster, method must be: "single", "complete",
-#     # "average", "mcquitty", "ward.D",
-#     # "ward.D2", "centroid" or "median".
-#
-#     R"library('fastcluster')"
-#
-#     if method == :single
-#         R"clusters <- hclust(as.dist(d), \"single\")"
-#     elseif method == :complete
-#         R"clusters <- hclust(as.dist(d), \"complete\")"
-#     elseif method == :average
-#         R"clusters <- hclust(as.dist(d), \"average\")"
-#     elseif method == :weighted
-#         R"clusters <- hclust(as.dist(d), \"mcquitty\")"
-#     elseif method == :ward
-#         R"clusters <- hclust(as.dist(d), \"ward.D2\")"
-#     elseif method == :ward1
-#         R"clusters <- hclust(as.dist(d), \"ward.D\")"
-#     elseif method == :centroid
-#         R"clusters <- hclust(as.dist(d), \"centroid\")"
-#     elseif method == :median
-#         R"clusters <- hclust(as.dist(d), \"median\")"
-#     else
-#         error("Invalid or unsupported method.")
-#     end
-#
-#     @rput nclusters
-#     R"clusterCut <- cutree(clusters, k = nclusters)"
-#     @rget clusterCut
-#
-#     return clusterCut
-#
-# end
+# answers[:single] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+# answers[:complete] = [1, 2, 2, 2, 1, 1, 2, 1, 2, 2, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 2, 1, 1, 1, 2, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 2, 1, 1, 2, 1, 2, 1, 1, 3, 1, 3, 2, 1, 2, 1, 2, 1, 2, 2, 1, 1, 1, 2, 3, 2, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 2, 1, 3, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 3, 1, 1, 3, 2, 3, 1, 3, 1, 1, 1, 2, 2, 1, 1, 3, 3, 1, 3, 2, 3, 1, 3, 3, 1, 1, 1, 3, 3, 3, 1, 1, 1, 3, 1, 1, 1, 3, 3, 3, 2, 3, 3, 1, 1, 1, 1, 1]
+# answers[:average] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 3, 2, 2, 3, 1, 3, 2, 3, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 3, 2, 2, 3, 2, 2, 2, 3, 3, 3, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+# answers[:weighted] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 2, 2, 1, 2, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 3, 2, 2, 3, 1, 3, 2, 3, 2, 2, 2, 1, 1, 2, 2, 3, 3, 2, 2, 1, 3, 2, 2, 3, 2, 2, 2, 3, 3, 3, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2]
+# answers[:centroid] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 3, 2, 2, 3, 1, 3, 2, 3, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 3, 2, 2, 3, 2, 2, 2, 3, 3, 3, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+# answers[:median] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 3, 2, 2, 3, 1, 3, 2, 3, 2, 2, 2, 1, 1, 2, 2, 3, 3, 2, 2, 1, 3, 2, 2, 3, 2, 1, 2, 3, 3, 3, 2, 2, 1, 3, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+# answers[:ward] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 2, 3, 2, 1, 2, 1, 1, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 1, 3, 2, 3, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 3, 2, 3, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 3]
 #
 # for meth in [:single, :complete, :average, :weighted, :centroid, :median]
 #     println("Testing interface with method=:$(string(meth))...")
-#     for k in [1 , 3, 5]
+#     for k in [3]
 #         # run once with R interface, and once with Fastcluster.jl interface
 #         points = convert(Array{Float64,2},df[:,[:SepalWidth, :SepalLength]])
 #         d = pairwise(Euclidean(), points, dims=1)
 #         m,h = Fastcluster.linkage(d, meth)
 #         c_fastcluster = Fastcluster.cutree(m,(length(m)>>1)+1,k)
-#         r_fastcluster = cluster_and_cut_R(d, meth, k)
-#         @test are_clusters_equal(c_fastcluster, r_fastcluster)
+#         #@show c_fastcluster
+#         @test are_clusters_equal(c_fastcluster, answers[meth])
 #     end
 #     println("Ok.")
 # end
@@ -145,15 +60,100 @@ end
 # # for
 # for meth in [:ward]
 #     println("Testing interface with method=:$(string(meth))...")
-#     for k in [1 , 3, 5]
+#     for k in [3]
 #         # run once with R interface, and once with Fastcluster.jl interface
 #         points = convert(Array{Float64,2},df[:,[:SepalWidth, :SepalLength]])
 #         d = pairwise(SqEuclidean(), points, dims=1)
 #         m,h = Fastcluster.linkage(d, meth)
 #         c_fastcluster = Fastcluster.cutree(m,(length(m)>>1)+1,k)
 #         d2 = pairwise(Euclidean(), points, dims=1)
-#         r_fastcluster = cluster_and_cut_R(d2, meth, k)
-#         @test are_clusters_equal(c_fastcluster, r_fastcluster)
+#         #@show c_fastcluster
+#         @test are_clusters_equal(c_fastcluster, answers[meth])
 #     end
 #     println("Ok.")
 # end
+
+
+
+R"r = getOption(\"repos\")
+r[\"CRAN\"] = \"http://cran.uk.r-project.org\"
+options(repos = r)
+rm(r)"
+R"install.packages(\"fastcluster\")"
+
+# function to wrap R interface:
+function cluster_and_cut_R(d::Array{T,2}, method::Symbol, nclusters::Int64) where {T<:Real}
+
+    @rput d
+
+    mthidx = Dict(:single => 0,
+              :complete  => 1,
+              :average   => 2,
+              :weighted  => 3,
+              :ward      => 4,
+              :centroid  => 5,
+              :median    => 6 )
+
+    # for R's fastcluster, method must be: "single", "complete",
+    # "average", "mcquitty", "ward.D",
+    # "ward.D2", "centroid" or "median".
+
+    R"library('fastcluster')"
+
+    if method == :single
+        R"clusters <- hclust(as.dist(d), \"single\")"
+    elseif method == :complete
+        R"clusters <- hclust(as.dist(d), \"complete\")"
+    elseif method == :average
+        R"clusters <- hclust(as.dist(d), \"average\")"
+    elseif method == :weighted
+        R"clusters <- hclust(as.dist(d), \"mcquitty\")"
+    elseif method == :ward
+        R"clusters <- hclust(as.dist(d), \"ward.D2\")"
+    elseif method == :ward1
+        R"clusters <- hclust(as.dist(d), \"ward.D\")"
+    elseif method == :centroid
+        R"clusters <- hclust(as.dist(d), \"centroid\")"
+    elseif method == :median
+        R"clusters <- hclust(as.dist(d), \"median\")"
+    else
+        error("Invalid or unsupported method.")
+    end
+
+    @rput nclusters
+    R"clusterCut <- cutree(clusters, k = nclusters)"
+    @rget clusterCut
+
+    return clusterCut
+
+end
+
+for meth in [:single, :complete, :average, :weighted, :centroid, :median]
+    println("Testing interface with method=:$(string(meth))...")
+    for k in [1 , 3, 5]
+        # run once with R interface, and once with Fastcluster.jl interface
+        points = convert(Array{Float64,2},df[:,[:SepalWidth, :SepalLength]])
+        d = pairwise(Euclidean(), points, dims=1)
+        m,h = Fastcluster.linkage(d, meth)
+        c_fastcluster = Fastcluster.cutree(m,(length(m)>>1)+1,k)
+        r_fastcluster = cluster_and_cut_R(d, meth, k)
+        @test are_clusters_equal(c_fastcluster, r_fastcluster)
+    end
+    println("Ok.")
+end
+
+# for
+for meth in [:ward]
+    println("Testing interface with method=:$(string(meth))...")
+    for k in [1 , 3, 5]
+        # run once with R interface, and once with Fastcluster.jl interface
+        points = convert(Array{Float64,2},df[:,[:SepalWidth, :SepalLength]])
+        d = pairwise(SqEuclidean(), points, dims=1)
+        m,h = Fastcluster.linkage(d, meth)
+        c_fastcluster = Fastcluster.cutree(m,(length(m)>>1)+1,k)
+        d2 = pairwise(Euclidean(), points, dims=1)
+        r_fastcluster = cluster_and_cut_R(d2, meth, k)
+        @test are_clusters_equal(c_fastcluster, r_fastcluster)
+    end
+    println("Ok.")
+end
